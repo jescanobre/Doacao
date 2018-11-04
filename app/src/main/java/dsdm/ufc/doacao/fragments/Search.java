11707db7a4a9570e13e9cef9444f295c1740b696
mariajesca <jescanobre@gmail.com>
2018-11-04 16:36:45 -0200

    up

---
+++ app/src/main/java/dsdm/ufc/doacao/fragments/Search.java
@@ -0,0 +1,34 @@
+package dsdm.ufc.doacao.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dsdm.ufc.doacao.R;
+
+public class Search extends Fragment {
+
+    public Search() {
+    }
+
+    public static Search newInstance() {
+        Search fragment = new Search();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+}
