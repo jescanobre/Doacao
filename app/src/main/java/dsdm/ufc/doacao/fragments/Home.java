29aea0146111e6ce1ab8f041e37f9c634eacd7d9
mariajesca <jescanobre@gmail.com>
2018-11-05 11:00:41 -0300

    up

--- app/src/main/java/dsdm/ufc/doacao/fragments/Home.java
+++ app/src/main/java/dsdm/ufc/doacao/fragments/Home.java
@@ -1,11 +1,14 @@
 package dsdm.ufc.doacao.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import dsdm.ufc.doacao.ObjetoDetalhe;
 import dsdm.ufc.doacao.R;
 
 
@@ -28,7 +31,55 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        CardView cardView1 = view.findViewById(R.id.card_view);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirObjDetalhe();
+            }
+        });
+
+        CardView cardView2 = view.findViewById(R.id.card_view1);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirObjDetalhe();
+            }
+        });
+
+        CardView cardView3 = view.findViewById(R.id.card_view2);
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirObjDetalhe();
+            }
+        });
+
+        CardView cardView4 = view.findViewById(R.id.card_view3);
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirObjDetalhe();
+            }
+        });
+
+        CardView cardView5 = view.findViewById(R.id.card_view4);
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirObjDetalhe();
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    public void abrirObjDetalhe (){
+        Intent intent = new Intent(getActivity(), ObjetoDetalhe.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/dsdm/ufc/doacao/fragments/User.java
+++ app/src/main/java/dsdm/ufc/doacao/fragments/User.java
@@ -1,14 +1,17 @@
 package dsdm.ufc.doacao.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import dsdm.ufc.doacao.R;
+import dsdm.ufc.doacao.meuObj;
 
 public class User extends Fragment {
 
@@ -28,7 +31,26 @@ public class User extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        ListView objLista = (ListView) view.findViewById(R.id.objetosLista);
+
+        final String lista[] = new String[]{"Cadeira de Balanço","Bicicleta"};
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,lista);
+        objLista.setAdapter(arrayAdapter);
+
+        objLista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(lista[position].equals("Cadeira de Balanço")){
+                    Intent i = new Intent(getActivity(),meuObj.class);
+
+                    startActivity(i);
+                }
+            }
+        });
+        return view;
     }
 
 }
