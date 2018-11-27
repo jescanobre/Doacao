71327c18ce7c845fbd76bb85c6c0090c5908ccc7
BrennoNog <brennonogueirao@gmail.com>
2018-11-27 14:32:11 -0300

    Search e perfil (usuario e obj) atualizando

--- app/src/main/java/dsdm/ufc/doacao/ObjetoDetalhe.java
+++ app/src/main/java/dsdm/ufc/doacao/ObjetoDetalhe.java
@@ -10,6 +10,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import dsdm.ufc.doacao.entidades.Objeto;
+import dsdm.ufc.doacao.entidades.Usuarios;
+
 public class ObjetoDetalhe extends AppCompatActivity {
 
 
@@ -20,6 +32,46 @@ public class ObjetoDetalhe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objeto_detalhe);
+
+        DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference("objeto");
+
+
+        Intent intent = getIntent();
+        String nome1 = intent.getExtras().getString("nome");
+
+        System.out.println("aaaaaaaaaaaaaaaaaaa   4" + nome1);
+        Query query1=mDatabaseRef.orderByChild("titulo").equalTo(nome1);
+
+
+        final TextView txtTitulo = (TextView) findViewById(R.id.txtTitulo);
+        final TextView txtDescricao = (TextView) findViewById(R.id.txtDescricao);
+
+
+        query1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot data:dataSnapshot.getChildren()){
+
+
+                    final Objeto models=data.getValue(Objeto.class);
+                    String nome=models.getTitulo();
+                    String descricao=models.getDescricao();
+                    txtTitulo.setText(nome);
+                    txtDescricao.setText(descricao);
+                    procuraId(models.getIdDoador());
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         euQuero = (Button)findViewById(R.id.euQuero);
 
@@ -33,15 +85,46 @@ public class ObjetoDetalhe extends AppCompatActivity {
             }
         });
 
-        usuarioNome = (TextView)findViewById(R.id.usuarioNome);
-        usuarioNome.setOnClickListener(new View.OnClickListener() {
+
+
+    }
+
+    public void procuraId(final String userID){
+        final DatabaseReference refUser = FirebaseDatabase.getInstance().getReference("usuario");
+        Query query2=refUser.orderByChild("id").equalTo(userID);
+        final TextView usuarioNome = (TextView) findViewById(R.id.usuarioNome);
+        query2.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ObjetoDetalhe.this,PerfilGeral.class);
-                startActivity(i);
-                finish();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot data:dataSnapshot.getChildren()){
+
+
+                    final Usuarios models=data.getValue(Usuarios.class);
+                    String nome=models.getNome();
+                    usuarioNome.setText(nome);
+
+
+
+                    usuarioNome.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(ObjetoDetalhe.this,PerfilGeral.class);
+                            i.putExtra("id",models.getId());
+                            startActivity(i);
+                        }
+                    });
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
 
     }
 }
